ec68a651b417b98d96b83af13a52d77b73e5d189
David Chan <dchan902@gmail.com>
2021-11-03 22:55:57 -0600

    MochaChai Test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,13 @@
 const request = require('request');
 
 const fetchBreedDescription = function(breedName, callback) {
-request(`https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`, (error, response, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`, (error, response, body) => {
     if (error) {
       callback(error, null);
     }
     const data = JSON.parse(body);
     if (!data[0]) {
-      callback(`Breed Not Found`, null)
+      callback(`Breed Not Found`, null);
     } else {
       callback(error, data[0]['description']);
     }
